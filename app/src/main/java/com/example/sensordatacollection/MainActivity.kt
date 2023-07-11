944d898db4aa10d38ea2a1bffa881da4c077b6c6
Dhruv Saxena <dhruv162002@gmail.com>
2023-07-11 12:43:24 +0530

    Add UI

--- app/src/main/java/com/example/sensordatacollection/MainActivity.kt
+++ app/src/main/java/com/example/sensordatacollection/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.sensordatacollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.dark_mode_bg)
     }
 }
